3afef679074810a2c3688d084d9bb2febfa746c3
mukeshkumar098 <mukeshkemar567@gmail.com>
2025-05-11 20:35:26 +0530

    update

--- frontend/src/page/contect-Us/contact.jsx
+++ frontend/src/page/contect-Us/contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
